720f54d110a6c068a58aeab25ddca3654c5b5f74
Arhen Santiago <lincoldarhen9900@gmail.com>
2022-09-02 16:55:10 +0800

    added: user dropdown

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,20 @@
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
 function Header() {
   return (
-    <Navbar bg="primary" variant="dark">
+    <Navbar bg="primary" variant="dark" collapseOnSelect expand="lg">
       <Container>
         <Navbar.Brand href="#home">ReactCM</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsiveNav" />
+        <Navbar.Collapse id="responsiveNav" className="justify-content-end">
+          <Nav navbarScroll>
+            <NavDropdown title="Account" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action4">Log Out</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
