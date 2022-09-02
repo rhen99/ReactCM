8b7a2574fd335b683a9f796a2f6555965900d189
Arhen Santiago <lincoldarhen9900@gmail.com>
2022-09-02 17:38:10 +0800

    added: google signin button

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,5 +1,29 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { Google } from "react-bootstrap-icons";
 function SignIn() {
-  return <div>SignIn</div>;
+  return (
+    <Container className="mt-5">
+      <Row className="mb-4">
+        <Col>
+          <h3 className="text-center">Sign In With:</h3>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Form className="text-center">
+            <Button variant="primary" size="lg" style={{ width: "50%" }}>
+              <Google color="white" className="mx-1 pb-1" />
+              Sign In With Google
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default SignIn;
