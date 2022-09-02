20237e249afd04df0d80f9188932b4cafb29bf7c
Arhen Santiago <lincoldarhen9900@gmail.com>
2022-09-02 16:32:55 +0800

    added: routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import SignIn from "./components/SignIn";
+import Dashboard from "./components/Dashboard";
 function App() {
   return (
-    <>
+    <Router>
       <Header />
-    </>
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,16 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+function Dashboard() {
+  return (
+    <Container className="mt-5">
+      <Row>
+        <Col>
+          <h1>Customer List</h1>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Dashboard;
