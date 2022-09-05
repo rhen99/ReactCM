20b1c757013b60f69178f235b41f7194ea940fb2
Arhen Santiago <lincoldarhen9900@gmail.com>
2022-09-05 14:53:46 +0800

    fixed: logout button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,11 @@ import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
 function Header() {
-  const { signOut } = useAuthContext();
+  const { signOut, isAuthenticated } = useAuthContext();
   const logOut = (e) => {
     e.preventDefault();
-    console.log(signOut());
+    signOut();
   };
   return (
     <Navbar bg="primary" variant="dark" collapseOnSelect expand="lg">
@@ -18,13 +16,13 @@ function Header() {
         <Navbar.Toggle aria-controls="responsiveNav" />
         <Navbar.Collapse id="responsiveNav" className="justify-content-end">
           <Nav navbarScroll>
-            <NavDropdown title="Account" id="navbarScrollingDropdown">
-              <Form onSubmit={logOut}>
-                <Button type="submit" variant="default">
+            {isAuthenticated && (
+              <NavDropdown title="Account" id="navbarScrollingDropdown">
+                <NavDropdown.Item href="#" onClick={logOut}>
                   Log Out
-                </Button>
-              </Form>
-            </NavDropdown>
+                </NavDropdown.Item>
+              </NavDropdown>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
